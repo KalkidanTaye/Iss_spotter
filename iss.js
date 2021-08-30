a3a0194822999fa3dd9300063dfe5cc4f43dafc8
Kalkidan Alemu <kali.taye@gmail.com>
2021-08-30 00:29:45 -0400

    Add fetchISSFlyover times and fetchCoordsByIP function

--- iss.js
+++ iss.js
@@ -27,12 +27,53 @@ const fetchMyIP = function (callback) {
   });
 };
 
+const fetchCoordsByIP = function (ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(
+        Error(
+          `Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`
+        ),
+        null
+      );
+      return;
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+const fetchISSFlyOverTimes = function (coords, callback) {
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`;
+
+  request(url, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(
+        Error(
+          `Status Code ${response.statusCode} when fetching ISS pass times: ${body}`
+        ),
+        null
+      );
+      return;
+    }
+
+    const passes = JSON.parse(body).response;
+    callback(null, passes);
+  });
+};
+
 module.exports = { fetchMyIP };
-
-// var http = require('http');
-
-// http.get({'host': 'api.ipify.org', 'port': 80, 'path': '/'}, function(resp) {
-//   resp.on('data', function(ip) {
-//     console.log("My public IP address is: " + ip);
-//   });
-// });
+module.exports = { fetchCoordsByIP };
+module.exports = { fetchISSFlyOverTimes };
